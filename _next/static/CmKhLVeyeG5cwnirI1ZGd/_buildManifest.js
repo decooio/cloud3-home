952953de248e19f5333181b7883a2f158395befe
lijun4639 <lijun4639@users.noreply.github.com>
2022-10-11 06:19:59 +0000

    deploy: c3e79ebda2f990635739b0a661d96d19677b7ddd

---
+++ _next/static/CmKhLVeyeG5cwnirI1ZGd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-c7ce1310b5ccf1ee.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

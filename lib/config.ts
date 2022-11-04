c66757238fc7d7a6b2c0adb30a0f5eae74799c37
eericxu <2681350846@qq.com>
2022-11-04 13:04:01 +0800

    fix test w3b address

--- lib/config.ts
+++ lib/config.ts
@@ -3,7 +3,7 @@ import { chain } from "wagmi";
 import { IS_DEV } from "./env";
 
 export const GA_ID = IS_DEV ? "-" : "-";
-export const W3Bucket_Adress: `0x${string}` = IS_DEV
+export const W3Bucket_Adress: `0x${string}` = IS_DEV || IS_TEST
   ? "0x398663842680332A1AbA3B03bd6dB47aE984994C"
   : "0x124";
 

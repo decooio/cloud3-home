8a7c3c04c184df3a87659e81edf9cc6ad1215a39
li4639 <346766377@qq.com>
2022-11-15 14:46:06 +0800

    test auth cache

--- lib/hooks/useListenerEthereumInit.ts
+++ lib/hooks/useListenerEthereumInit.ts
@@ -3,27 +3,39 @@ import {useEffect, useState} from "react";
 export function useListenerEthereumInit() {
   const [isInit,setIsInit] = useState(false)
   useEffect(()=>{
-    if ((window as Window).ethereum) {
-      // alert(1)
-      console.info(window)
-      console.info(window?.ethereum)
-      setIsInit(true)
-    } else {
-      console.info("xia")
-      console.info(window)
-      window.addEventListener(
-        'ethereum#initialized',
-        ()=>{
-          console.info('ethereum---xxxxx')
-          setIsInit(true)
-        },
-        { once: true },
-      );
-      setTimeout(() => {
-        // handleEthereum();
+    // if ((window as Window).ethereum) {
+    //   // alert(1)
+    //   console.info(window)
+    //   console.info(window?.ethereum)
+    //   setIsInit(true)
+    // } else {
+    //   console.info("xia")
+    //   console.info(window)
+    //   window.addEventListener(
+    //     'ethereum#initialized',
+    //     ()=>{
+    //       console.info('ethereum---xxxxx')
+    //       setIsInit(true)
+    //     },
+    //     { once: true },
+    //   );
+    //   setTimeout(() => {
+    //     // handleEthereum();
+    //     setIsInit(true)
+    //   }, 3000);
+    // }
+    window.addEventListener(
+      'ethereum#initialized',
+      ()=>{
+        console.info('ethereum---xxxxx')
         setIsInit(true)
-      }, 3000);
-    }
+      },
+      { once: true },
+    );
+    setTimeout(() => {
+      // handleEthereum();
+      setIsInit(true)
+    }, 3000);
   },[])
   return [isInit,setIsInit]
 }

b65af211115ba0b548bcfd034937dc7ede40b466
li4639 <346766377@qq.com>
2022-11-15 11:43:31 +0800

    test auth cache

--- lib/hooks/useGetAuth.ts
+++ lib/hooks/useGetAuth.ts
@@ -23,6 +23,10 @@ export function useGetAuth(
     const current = moment().unix();
     if (cache && old) {
       const lastAuth = JSON.parse(window.atob(old)).data;
+      console.log(lastAuth)
+      console.log(tokenID)
+      console.log(lastAuth.message.tokenID)
+      console.log(lastAuth.message.expirationTimestamp - current > 300)
       if (
         lastAuth.domain.chainId === `${chainId}` &&
         lastAuth.message.signingAddress === address &&

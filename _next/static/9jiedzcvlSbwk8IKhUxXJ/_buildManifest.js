fda584568e83034f6d190525ac0048d3fbc6c4ac
lijun4639 <lijun4639@users.noreply.github.com>
2022-12-13 09:12:48 +0000

    deploy: 942d3536d9d09b8381584077650fae325076d37e

--- _next/static/9jiedzcvlSbwk8IKhUxXJ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-aff7f19ce10e06b8.js"],"/_error":["static/chunks/pages/_error-409f831d3504c8f5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

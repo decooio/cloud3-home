32073c9bcda9c7ebb8da027d5d25827ab4eeb3f2
lijun4639 <lijun4639@users.noreply.github.com>
2022-12-13 08:53:47 +0000

    deploy: 599aaf52f4de6532c6f0f472a4f99e5d2b999d40

---
+++ _next/static/9jiedzcvlSbwk8IKhUxXJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-aff7f19ce10e06b8.js"],"/_error":["static/chunks/pages/_error-409f831d3504c8f5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/vzP1-fvxjkYqSVpJ2ip2C/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-034a2f1eb4074eb1.js"],"/_error":["static/chunks/pages/_error-409f831d3504c8f5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

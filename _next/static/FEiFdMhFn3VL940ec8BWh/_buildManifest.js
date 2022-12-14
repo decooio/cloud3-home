3b8ab915e2d9ba4eae5d59650454bd41079106ab
lijun4639 <lijun4639@users.noreply.github.com>
2022-12-14 03:45:52 +0000

    deploy: 5a6009e4af8f514cd9e3effbc0938cc544994ce4

--- _next/static/FEiFdMhFn3VL940ec8BWh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-14f3fd28e08d7ca7.js"],"/_error":["static/chunks/pages/_error-409f831d3504c8f5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

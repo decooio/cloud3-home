7e1e3bf1f3765d9a4687e3f0a96db637c69378d4
li4639 <346766377@qq.com>
2022-12-15 16:32:01 +0800

    fix word

--- components/pages/home/SectionTop.tsx
+++ components/pages/home/SectionTop.tsx
@@ -32,8 +32,8 @@ export const SectionTop = React.memo(() => {
           </div>
           <div className="h-full my-10 w-[69.5rem] flex flex-col justify-center mt-[-1rem]">
             <div className="text-5xl leading-tight">
-              <p>Store in IPFS W3Bucket,</p>
-              <p>Decentralized, Guaranteed & Alive.</p>
+              <p>Cloud 3</p>
+              <p>Redefining Web3 Storage</p>
             </div>
             <div className="flex mt-12 pl-1">
               <Button
